ef2127e06e1ca8188ba42ca95c2d76d8ffa561ce
samwhindleton <samwhindleton@gmail.com>
2018-04-25 04:16:21 -0700

    [1] added react, babel cdn to index.html, [2] created main.css and app.js

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Test.<br/>Do you like our app?<br/>Is beautiful right?<br/>More to come very soon.<br/>Hang on tight!<br/>It's going to be fantastic!!!</h1>
+      </div>
+    )
+  };
+};
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector(".container")
+);
