3e3e407588fe84da494be7baef1b6da3cf58e923
Erin McMackin <mcmackinerin@gmail.com>
2018-04-25 17:00:29 -0400

    show page working; able to click edit button on show page to render edit static page

--- public/js/app.js
+++ public/js/app.js
@@ -9,11 +9,48 @@ class Header extends React.Component{
   }
 }
 
+// displays the index
 class CommunityImagesList extends React.Component{
   render(){
     return(
       <div className="community_image_container">
-        pics go here
+        {this.props.boardData.map((image, index)=>{
+          return(
+            <img
+              src={image.image}
+              alt={image.description}
+              onClick={()=>{
+                this.props.getCommunityImage(image);
+              }}
+            />
+          )
+        })}
+      </div>
+    )
+  }
+}
+
+// displays the show image
+class CommunityImage extends React.Component{
+  render(){
+    return(
+      <div className="comm_board_show">
+        <h1>{this.props.image.description}</h1>
+        <img src={this.props.image.image} alt={this.props.image.description} />
+        <button onClick={()=>{
+          this.props.showEditPage(this.props.image)
+        }}>Edit</button>
+        <button>Back to List</button>
+      </div>
+    )
+  }
+}
+
+class EditImage extends React.Component{
+  render(){
+    return(
+      <div className="comm_image_edit_container">
+        edit page
       </div>
     )
   }
@@ -23,15 +60,23 @@ class CommunityBoard extends React.Component{
   constructor(props){
     super(props);
     this.state = ({
-      communityImages: []
+      communityImages: [],
+      image: '',
+      indexVisible: true,
+      showVisible: false,
+      editVisible: false
     });
     this.getCommunityImages = this.getCommunityImages.bind(this);
+    this.getCommunityImage = this.getCommunityImage.bind(this);
+    this.showEditPage = this.showEditPage.bind(this);
   }
 
+  // runs getCommunityImages on load
   componentDidMount(){
    this.getCommunityImages();
   }
 
+  // fetches the community_boards data from the db
   getCommunityImages(){
     fetch("/community_boards")
       .then((response)=>response.json())
@@ -47,12 +92,50 @@ class CommunityBoard extends React.Component{
       })
   }
 
+  // hide index, show image show component
+  getCommunityImage(image){
+    this.setState({
+      image: image,
+      indexVisible: false,
+      showVisible: true
+    })
+  }
+
+  // show edit component
+  showEditPage(image){
+    this.setState({
+      showVisible: false,
+      editVisible: true
+    })
+    console.log(this.state.image);
+  }
+
   render(){
     return(
       <div className="comm_board_container">
         {
-          (this.state.communityImages)?
-            <CommunityImagesList />
+          (this.state.communityImages && this.state.indexVisible)?
+            <CommunityImagesList
+              boardData={this.state.communityImages}
+              getCommunityImage={this.getCommunityImage}
+            />
+          :
+            ''
+        }
+        {
+          (this.state.showVisible)?
+            <CommunityImage
+              image={this.state.image}
+              showEditPage={this.showEditPage}
+            />
+          :
+            ''
+        }
+        {
+          (this.state.editVisible)?
+            <EditImage
+              image={this.state.image}
+            />
           :
             ''
         }
