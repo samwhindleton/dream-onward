6d617f4c3fc74ac84591eaa96627160787e1a66e
Erin McMackin <mcmackinerin@gmail.com>
2018-04-25 15:47:07 -0400

    CommunityBoard and Header class created, app rendering both

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,78 @@
-class App extends React.Component {
-  render() {
-    return (
+class Header extends React.Component{
+  render(){
+    return(
+      <header className="header">
+        <h1>Dream Onward</h1>
+        <h2>Paper-Free Dream Boards</h2>
+      </header>
+    )
+  }
+}
+
+class CommunityImagesList extends React.Component{
+  render(){
+    return(
+      <div className="community_image_container">
+        pics go here
+      </div>
+    )
+  }
+}
+
+class CommunityBoard extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = ({
+      communityImages: []
+    });
+    this.getCommunityImages = this.getCommunityImages.bind(this);
+  }
+
+  componentDidMount(){
+   this.getCommunityImages();
+  }
+
+  getCommunityImages(){
+    fetch("/community_boards")
+      .then((response)=>response.json())
+      // .then((data)=>console.log(data))
+      .then((data)=>{
+        console.log(data);
+        this.setState({
+          communityImages: data
+        })
+      })
+      .catch((error)=>{
+        console.log(error);
+      })
+  }
+
+  render(){
+    return(
+      <div className="comm_board_container">
+        {
+          (this.state.communityImages)?
+            <CommunityImagesList />
+          :
+            ''
+        }
+      </div>
+    )
+  }
+}
+
+class App extends React.Component{
+  render(){
+    return(
       <div>
-        <h1>Test.<br/>Do you like our app?<br/>Is beautiful right?<br/>More to come very soon.<br/>Hang on tight!<br/>It's going to be fantastic!!!</h1>
+        <Header />
+        <CommunityBoard />
       </div>
     )
   };
 };
 
 ReactDOM.render(
-  <App/>,
-  document.querySelector(".container")
-);
+  <App />,
+  document.querySelector('.container')
+)
