8604d390dcb433b6087653f8955f5bb591a5489b
Brian R Murphy <BrianRMurphy89@gmail.com>
2018-04-24 21:16:42 -0400

    Route and Controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/community_board', to: 'community_board#index'
+  get '/community_board/:id', to: 'coummunity_board#show'
+  post '/community_board', to: 'community_board#create'
+  delete '/community_board/:id', to: 'community_board#delete'
+  put '/community_board/:id', to: 'community_board#update'
 end
